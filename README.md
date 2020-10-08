7da52da7d6f41de3bcb5be4ac856e56cdc96c9ab
[zu0p] <a33a66a99@naver.com>
2020-10-08 22:07:28 +0900

    create add todo

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Todo list 
-React JS
+## ReactJS | styled-components | Reack Hook
--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,14 +1,27 @@
 import React,{useState} from 'react'
 import styled from 'styled-components'
+import axios from 'axios'
 
 const AddForm = () => {
+    const [text, setText]=useState("")
+
+    const handleChange=(e)=>{
+        setText(e.target.value)
+    }
+
+    async function handleClick(){
+        await axios.post("http://localhost:8282/todo/create_todo",{
+            contents: text,
+            id: "teo"   //user id
+        })
+        window.location.replace("/")    
+    }
+
     return (
         <div>
             <AddFromWrapper>
-                <AddBox>
-
-                </AddBox>
-                <AddButton>+</AddButton>
+                <AddBox onChange={handleChange} value={text} />
+                <AddButton onClick={handleClick}>+</AddButton>
             </AddFromWrapper>
         </div>
     );
@@ -16,7 +29,9 @@ const AddForm = () => {
 
 const AddFromWrapper=styled.div``
 
-const AddBox=styled.textarea``
+const AddBox=styled.input.attrs({
+    type : 'text',
+})``
 
 const AddButton=styled.button``
 
