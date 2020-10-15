48e7bfef5381ead636ae80e2568e8dd7dbea4804
[zu0p] <a33a66a99@naver.com>
2020-10-16 00:07:41 +0900

    fix kakao login

---
+++ src/components/OAuth/KakaoLoginForm.js
@@ -0,0 +1,72 @@
+import React, {useState} from 'react'
+import KakaoLogin from 'react-kakao-login'
+import styled from 'styled-components'
+import axios from 'axios'
+import url from '../../share/url'
+import kakaoKey from '../../share/kakao'
+
+const KakaoLoginForm =()=>{
+    const responseKakao = async result =>{
+        const {profile, response} = result
+        const {id, kakao_account, properties} = profile
+        const {access_token, refresh_token} = response
+        const {nickname, profile_image} = properties
+        const {email} = kakao_account
+
+        const body = {
+            id,
+            name: nickname,
+            email: email,
+            profileUrl: profile_image
+        }
+        const headers = {
+            "access-token": access_token,
+            "refresh-token": refresh_token,
+            "Content-Type": "application/json"
+        }
+
+        const res = await axios.post(`${url}/user/kakao`, headers, body)
+        const {success, data} = res
+        if(success==true){
+            localStorage.setItem("access-token", data.accessToken)
+            localStorage.setItem("refresh-token", data.refreshToken)
+            window.location.reload()
+        }else alert("카카오 로그인에 실패하였습니다.")
+    }
+ 
+    const responseFail = (err) => {
+        console.log(err)
+    }
+
+    return (
+        <>
+            <KakaoBtn
+                jsKey={kakaoKey}
+                onSuccess={responseKakao}
+                onFailure={responseFail}  
+                getProfile="true"
+                buttonText="Login with Kakao"
+            />
+        </>
+    )
+}
+
+const KakaoBtn=styled(KakaoLogin)`
+    padding: 0;
+    width: 190px;
+    height: 44px;
+    line-height: 44px;
+    color: #783c00;
+    background-color: #FFEB00;
+    border: 1px solid transparent;
+    border-radius: 3px;
+    font-size: 16px;
+    font-weight: bold;
+    text-align: center;
+    cursor: pointer;
+    &:hover{
+        box-shadow: 0 0px 15px 0 rgba(0, 0, 0, 0.2)
+    }
+`
+
+export default KakaoLoginForm
